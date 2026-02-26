9d019ed67133e78ab6c8e5c54b1ef1ba8281dd78
ddd822 <238666591+ddd822@users.noreply.github.com>
2026-02-26 02:54:39 +0000

    layoutメソッドで分ける

---
+++ app/controllers/public/shops_controller.rb
@@ -0,0 +1,3 @@
+class Public::ShopsController < ApplicationController
+  layout "shop"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :shops
+  devise_for :shops, path: 'shops', controllers: {
+  sessions: 'public/shops/sessions',
+  registrations: 'public/shops/registrations'
+}
+
+devise_for :customers, path: 'customers', controllers: {
+  sessions: 'public/customers/sessions',
+  registrations: 'public/customers/registrations'
+}
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
