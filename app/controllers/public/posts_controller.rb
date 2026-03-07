b47dc160dad1854d05e72570d2a94cb32b32bfaf
ddd822 <238666591+ddd822@users.noreply.github.com>
2026-03-07 13:36:38 +0000

    投稿機能（削除・編集）作成

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,6 +1,8 @@
 class Public::PostsController < ApplicationController
   before_action :authenticate_shop!, only: [:create], if: -> { shop_signed_in? }
   before_action :authenticate_customer!, only: [:create], if: -> { customer_signed_in? }
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_post!, only: [:edit, :update, :destroy]
 
   def new
     @post = Post.new
@@ -8,7 +10,6 @@ class Public::PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-
     if shop_signed_in?
       @post.postable = current_shop
     elsif customer_signed_in?
@@ -16,23 +17,34 @@ class Public::PostsController < ApplicationController
     else
       redirect_to root_path, alert: "ログインしてください" and return
     end
-
     if @post.save
       redirect_to @post, notice: "投稿が作成されました"
     else
-      render :new
+      render :new, alert: "ログインしてください。"
     end
   end
-
+  
   def index
     @posts = Post.order(created_at: :desc)
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: "投稿を更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: "投稿を削除しました。"
   end
 
   private
@@ -40,4 +52,17 @@ class Public::PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:title, :body)
   end
+
+  def set_post
+    @post = Post.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+    redirect_to posts_path, alert: "投稿が見つかりません。"
+  end
+
+  def authorize_post!
+    unless @post.postable == current_postable
+      redirect_to posts_path, alert: "編集・削除の権限がありません。"
+    end
+  end
+
 end
