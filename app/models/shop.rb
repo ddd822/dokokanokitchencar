9f9d32a79437bb53f1f352aa1627accaeb840377
ddd822 <238666591+ddd822@users.noreply.github.com>
2026-03-20 12:55:07 +0000

    shopユーザーedit画面項目追加

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -6,4 +6,6 @@ class Shop < ApplicationRecord
 has_many :posts, as: :postable, dependent: :destroy
 has_many :comments, as: :user, dependent: :destroy
 validates :name, presence: true, uniqueness: true, length: { maximum: 30 }
+validates :telephone_number, length: { maximum: 11 }
+validates :introduction, length: { maximum: 300 }
 end
